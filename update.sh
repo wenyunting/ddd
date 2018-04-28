f681dc28b28b42755584863b639b30b24d07e087
readr <38578840+wenyunting@users.noreply.github.com>
2018-04-28 11:11:21 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,12 +1,9 @@
 ﻿#!/bin/bash
 echo "欢迎使用网页雷达一键脚本"
 echo "请输入你的内网ip" 
-read -p "内网ip： " ip
-sudo su 
-chmod -R 777 /root
+read -p "内网ip： " ip 
 cp /root/ddd/restart.sh /root/restart.sh
 chmod +x restart.sh
-
 yum update -y
 yum install -y wget net-tools gcc make gdb python openssh-server tar epel-release ntp openssh-clients tar curl rsync bzip2 tcpdump less telnet lsof sysstat cronie python-setuptools gcc-c++ flex bison which man
 wget https://download.libsodium.org/libsodium/releases/LATEST.tar.gz -O /opt/LATEST.tar.gz
@@ -16,7 +13,6 @@ cd /opt && tar xvf libpcap.tar.gz && cd libpcap* && ./configure && make && make 
 curl https://raw.githubusercontent.com/creationix/nvm/v0.13.1/install.sh | bash && source ~/.bash_profile && nvm install v9.8.0 && nvm alias default v9.8.0
 npm i -g npm
 nvm install v10.0.0
-
 cd /root/ddd
 git clone https://github.com/wenyunting/ddd.git
 cd /root/ddd
@@ -24,13 +20,11 @@ npm i
 npm i -g pino
 npm install -g forever
 forever start index.js sniff eth0 $ip | pino
-
 wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
 chmod +x shadowsocks-all.sh
 ./shadowsocks-all.sh 2>&1 | tee shadowsocks-all.log
 
 echo "ss搭建成，请记住连接信息"
 read -p "记住了吗？任意键继续" 
-
 
 echo "搭建完成"
