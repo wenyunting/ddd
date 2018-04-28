c160024b6f4c1216997d40a655152563b71f23e4
readr <38578840+wenyunting@users.noreply.github.com>
2018-04-28 11:29:13 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -19,8 +19,5 @@ npm i
 npm i -g pino
 npm install -g forever
 forever start index.js sniff eth0 $ip | pino
-wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
-chmod +x shadowsocks-all.sh
-./shadowsocks-all.sh 2>&1 | tee shadowsocks-all.log
 
-echo "ss搭建成，请记住连接信息"
+echo
